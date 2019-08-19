170a03bf8089756d3641c9faf340ea5419855597
Jackmc <jackmc1122@gmail.com>
2019-08-19 10:15:07 -0700

    Make a test for the post route.

--- test/app.test.js
+++ test/app.test.js
@@ -17,4 +17,17 @@ describe('app routes', () => {
   afterAll(() => {
     return mongoose.connection.close();
   });
+
+  it('creates and returns a note', () => {
+    return request(app)
+      .post('/api/v1/notes')
+      .send({ title: 'test title', text: 'test text' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          title: 'test title',
+          text: 'test text'
+        });
+      });
+  });
 });
