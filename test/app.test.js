0c9e43d3837942de41b5d9326dcdd59e6f00539b
Jackmc <jackmc1122@gmail.com>
2019-08-19 11:39:40 -0700

    Add get route and test.

--- test/app.test.js
+++ test/app.test.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const request = require('supertest');
 const app = require('../lib/app');
+const Note = require('../lib/models/Note');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 
@@ -27,6 +28,23 @@ describe('app routes', () => {
           _id: expect.any(String),
           title: 'test title',
           body: 'test body'
+        });
+      });
+  });
+
+  it('returns all notes', async() => {
+    await Note.create([
+      { title: 'test title', body: 'test body' },
+      { title: 'Second note', body: 'second note body' }
+    ]);
+    return request(app)
+      .get('/api/v1/notes')
+      .then(res => {
+        expect(res.body).toHaveLength(2);
+        expect(res.body[0]).toEqual({
+          _id: expect.any(String),
+          title: expect.any(String),
+          body: expect.any(String)
         });
       });
   });
